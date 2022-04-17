8246c00fc68e689ff2e04d8a242a4997f7fad574
Lindsey White <LindseyWhite053@Gmail.com>
2022-04-17 03:24:37 -0400

    password and minimum variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,29 @@
-// Assignment code here
+function generatePassword() {
+// empty string variable for the for loop below
+var password = "password";
+
+var minNumbers = ""
+var minLowerCase = ""
+var minUpperCase = ""
+var minSpecialCharacters = ""
+
+//number of characters prompt
+var promptPasswordMin = window.prompt ("Please enter the number of characters you want for you new password.  It must be more than 8 but less than 128.");
+
+//lower case characters prompt
+var promptPasswordMax = window.confirm ("Include lower case characters in your password?");
+
+//uppercase characters prompt
+var promptPasswordMax = window.confirm ("Include uppercase characters in your password?");
+
+//special characters prompt
+var promptPasswordMax = window.confirm ("Include special characters in your password?");
+
+
+//return generated password
+return password;
+};
+
 
 
 // Get references to the #generate element
